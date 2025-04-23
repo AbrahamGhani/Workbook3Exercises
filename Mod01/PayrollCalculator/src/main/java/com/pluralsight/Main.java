d7b47b568ba6a5de0bb9ec493405179b95b4bc60
Abraham Ghani <AbrahamKosmoGhani@gmail.com>
2025-04-23 16:59:51 -0400

    Improved PayRollCalculator in Mod01

--- Mod01/PayrollCalculator/src/main/java/com/pluralsight/Main.java
+++ Mod01/PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -1,14 +1,41 @@
 package com.pluralsight;
 
-import java.util.List;
-
+import java.util.*;
+import java.io.*;
 public class Main {
+static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        List<Employee> employeeList = ReadFiles.readEmployeeCSV("employees.csv");
 
-        displayListOfEmployeesAndDetails(employeeList);
+   //     List<Employee> employeeList = ReadFiles.readEmployeeCSV("employees.csv");
 
+   //     displayListOfEmployeesAndDetails(employeeList);
+
+
+        try {
+            System.out.println("Enter name of file to process: ");
+            String fileToProcess = scanner.nextLine();
+            System.out.println("Enter name of file to write into: ");
+            String fileToWrite = scanner.nextLine();
+
+            List<Employee> newEmployeeList = ReadFiles.readEmployeeCSV(fileToProcess);
+
+            FileWriter fileWriter = new FileWriter(fileToWrite);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            String line;
+
+
+            for (Employee e : newEmployeeList){
+                line = e.getEmployeeID() + "|" + e.getName() + "|" + e.getHoursWorked() + "|" + e.getPayRate();
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+
+
+        }catch (IOException e){
+            System.out.println("ERROR");
+        }
 
 
     }
@@ -21,4 +48,6 @@ public class Main {
         }
         System.out.println("=======================");
     }
+
+
 }
---
+++ Mod01/PayrollCalculator/src/main/java/com/pluralsight/WriteIntoFiles.java
@@ -0,0 +1,59 @@
+package com.pluralsight;
+import java.io.*;
+import java.util.*;
+
+public class WriteIntoFiles {
+
+
+/*
+example i got from my dad
+NOTE TO SELF -- improve method to maybe prompt user for details and use that to write the file like i have in the main method
+ */
+
+public static void addEmployeeInFile(String _filename, List<Employee> _employees){
+
+    try {
+        FileWriter fileWriter = new FileWriter(_filename);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        String line;
+        for (Employee e : _employees){
+            line = e.getEmployeeID() + "|" + e.getName() + "|" + e.getHoursWorked() + "|" + e.getPayRate();
+            bufferedWriter.write(line);
+            bufferedWriter.newLine();
+        }
+        bufferedWriter.close();
+
+    }catch (IOException e){
+        System.out.println("IOException");
+    }
+
+
+
+
+
+
+
+
+//              //Example I am referencing
+//    try {
+//        // create a FileWriter
+//        FileWriter fileWriter = new FileWriter("text.txt");
+//        // create a BufferedWriter
+//        BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+//        // write to the file
+//        String text;
+//        for(int i = 1; i <= 10; i++) {
+//            text = String.format("Counting %d\n", i);
+//            bufWriter.write(text);
+//        }
+//        // close the writer
+//        bufWriter.close();
+//    }
+//    catch (IOException e) {
+//        System.out.println("ERROR: An unexpected error occurred");
+//        e.getStackTrace();
+//    }
+}
+
+
+}
